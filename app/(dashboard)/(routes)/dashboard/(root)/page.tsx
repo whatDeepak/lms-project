0e08abac58c190160bc3c2b249cf735430415dfd
Deepak <deekay9357@gmail.com>
2024-03-30 17:36:19 +0530

    initial-commit-current

---
+++ app/(dashboard)/(routes)/dashboard/(root)/page.tsx
@@ -0,0 +1,20 @@
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { LoginButton } from "@/components/auth/login-button";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"]
+})
+
+export default function Dashboard() {
+  return (
+    <>
+      <p>
+        Hello world!!
+      </p>
+    </>
+  )
+}
