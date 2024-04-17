843413d74d3a4c864729beb7efec0f7a26d6bef1
Gautam Sodhi <129111379+Iam-Sodhi@users.noreply.github.com>
2024-04-18 01:30:18 +0530

    "designing the course in the temp"

--- app/temp/page.tsx
+++ app/temp/page.tsx
@@ -1,75 +1,43 @@
-import { Metadata } from "next"
-import Image from "next/image"
+import { Metadata } from "next";
+import Image from "next/image";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from "@/components/ui/tabs"
-import { MainNav } from "./components/main-nav"
-import { Overview } from "./components/overview"
-import { RecentSales } from "./components/recent-sales"
-import { Search } from "./components/search"
-import { UserNav } from "./components/user-nav"
-import { CalendarDateRangePicker } from "./components/date-range-picker"
+} from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { MainNav } from "./components/main-nav";
+import { Overview } from "./components/overview";
+import { RecentSales } from "./components/recent-sales";
+import { Search } from "./components/search";
+import { UserNav } from "./components/user-nav";
+import { CalendarDateRangePicker } from "./components/date-range-picker";
+import DoughnutChart from "./components/doughnutChart";
 
 export const metadata: Metadata = {
   title: "Dashboard",
   description: "Example dashboard app built using the components.",
-}
+};
 
 export default function DashboardPage() {
   return (
     <>
-      <div className="md:hidden">
-        <Image
-          src="/examples/dashboard-light.png"
-          width={1280}
-          height={866}
-          alt="Dashboard"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/dashboard-dark.png"
-          width={1280}
-          height={866}
-          alt="Dashboard"
-          className="hidden dark:block"
-        />
-      </div>
+
       <div className="hidden flex-col md:flex">
         <div className="flex-1 space-y-4 p-4 pt-3">
-          <div className="flex items-center justify-between space-y-2">
-            <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
-            <div className="flex items-center space-x-2">
-              <CalendarDateRangePicker />
-              <Button>Download</Button>
-            </div>
-          </div>
-          <Tabs defaultValue="overview" className="space-y-4">
-            <TabsList>
-              <TabsTrigger value="overview">Overview</TabsTrigger>
-              <TabsTrigger value="notifications" disabled>
-                Notifications
-              </TabsTrigger>
-            </TabsList>
-            <TabsContent value="overview" className="space-y-4">
-              <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+          <div className="flex  space-x-2 justify-between ">
+            <div className="flex flex-col items-centre justify-start space-y-2 w-full">
+              <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
+              <div className="grid gap-4 md:grid-cols-2 ">
                 <Card>
                   <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                     <CardTitle className="text-sm font-medium">
                       Completed Courses
                     </CardTitle>
-
                   </CardHeader>
                   <CardContent>
                     <div className="text-2xl font-bold">25</div>
@@ -91,9 +59,8 @@ export default function DashboardPage() {
                     </p>
                   </CardContent>
                 </Card>
-       
               </div>
-              <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
+              <div className="grid gap-4 md:grid-cols-2 ">
                 <Card className="col-span-4">
                   <CardHeader>
                     <CardTitle>Overview</CardTitle>
@@ -102,22 +69,30 @@ export default function DashboardPage() {
                     <Overview />
                   </CardContent>
                 </Card>
-                <Card className="col-span-3">
-                  <CardHeader>
-                    <CardTitle>Recent Sales</CardTitle>
-                    <CardDescription>
-                      You made 265 sales this month.
-                    </CardDescription>
-                  </CardHeader>
-                  <CardContent>
-                    <RecentSales />
-                  </CardContent>
-                </Card>
               </div>
-            </TabsContent>
-          </Tabs>
+            </div>
+            <div className="flex flex-col items-center  space-y-2 justify-start">
+              <div className="flex items-center space-x-2">
+                <CalendarDateRangePicker />
+              </div>
+              <div className="h-[300px]">
+                <DoughnutChart />
+              </div>
+              {/* <Card className="col-span-3">
+                <CardHeader>
+                  <CardTitle>Recent Sales</CardTitle>
+                  <CardDescription>
+                    You made 265 sales this month.
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <RecentSales />
+                </CardContent>
+              </Card> */}
+            </div>
+          </div>
         </div>
       </div>
     </>
-  )
+  );
 }
