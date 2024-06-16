ed56e0a0d87aea575e86877cc3509ed5c6a5b62c
Gautam Sodhi <129111379+Iam-Sodhi@users.noreply.github.com>
2024-06-17 03:24:01 +0530

    courses and chapters pages is being done

--- app/api/courses/[courseId]/chapters/[chapterId]/progress/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/progress/route.ts
@@ -1,14 +1,16 @@
-import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 import { db } from "@/lib/db";
+import { currentUser } from "@/lib/auth";
 
 export async function PUT(
   req: Request,
   { params }: { params: { courseId: string; chapterId: string } }
 ) {
   try {
-    const { userId } = auth();
+    const user = await currentUser();
+    let userId = user?.id ?? "";
+  
     const { isCompleted } = await req.json();
 
     if (!userId) {
