a5cd01d351fd6c41cfb15ca1dff8f4beb0c10bd3
Deepak Kumar <139503599+whatDeepak@users.noreply.github.com>
2024-06-20 02:58:06 +0530

    vercel fixes @14

--- components/auth/error-card.tsx
+++ components/auth/error-card.tsx
@@ -8,6 +8,7 @@ export const ErrorCard = () => {
       headerLabel="Oops! Something went wrong!"
       backButtonHref="/auth/register"
       backButtonLabel="Back to register"
+      type="error"
     >
       <div className="w-full flex justify-center items-center">
       <ExclamationTriangleIcon className="text-destructive" />
