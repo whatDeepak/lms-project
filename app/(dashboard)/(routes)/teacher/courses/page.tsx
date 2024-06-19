d237ecb6879ab3764ddefa1b44f1bfabf8e58994
Deepak Kumar <139503599+whatDeepak@users.noreply.github.com>
2024-06-19 23:45:18 +0530

    Courses Data Table

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,16 +1,33 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
-const CoursesPage = () => {
-  return (
+import { db } from "@/lib/db";
+
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
+import { currentUser } from "@/lib/auth";
+
+const CoursesPage = async () => {
+  const user = await currentUser();
+  let userId = user?.id ?? "";
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return ( 
     <div className="p-6">
-      <Link href="/teacher/create">
-        <Button>
-          New Course
-        </Button>
-      </Link>
+      <DataTable columns={columns} data={courses} />
     </div>
-  );
+   );
 }
-
+ 
 export default CoursesPage;
