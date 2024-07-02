6227bfb7ac76ffb8d4545de60fbcf4817490bea3
Deepak Kumar <139503599+whatDeepak@users.noreply.github.com>
2024-07-03 00:50:13 +0530

    quiz page temp made

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/quizzes/[quizId]/page.tsx
@@ -0,0 +1,118 @@
+import { redirect } from "next/navigation";
+import Link from "next/link";
+import { ArrowLeft, Pencil, PlusCircle } from "lucide-react";
+
+import { db } from "@/lib/db";
+import { IconBadge } from "@/components/icon-badge";
+import { Banner } from "@/components/banner";
+
+import { QuizTitleForm } from "./_components/quiz-title-form";
+import { QuizQuestionsForm } from "./_components/quiz-question-form";
+//import { QuizActions } from "./_components/quiz-actions";
+import { currentUser } from "@/lib/auth";
+
+const QuizIdPage = async ({
+  params
+}: {
+  params: { courseId: string; chapterId: string; quizId: string }
+}) => {
+  const user = await currentUser();
+  let userId = user?.id ?? "";
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const quiz = await db.quiz.findUnique({
+    where: {
+      id: params.quizId, // Ensure quizId is properly received and used here
+    },
+    include: {
+      questions: true,
+    },
+  });
+
+  if (!quiz) {
+    return redirect("/")
+  }
+
+  const requiredFields = [
+    quiz.title,
+    quiz.description,
+    // Add any other required fields here
+  ];
+
+  const totalFields = requiredFields.length;
+  const completedFields = requiredFields.filter(Boolean).length;
+
+  const completionText = `(${completedFields}/${totalFields})`;
+
+  const isComplete = requiredFields.every(Boolean);
+
+  return (
+    <>
+      {!quiz.isPublished && (
+        <Banner
+          variant="warning"
+          label="This quiz is unpublished. It will not be visible in the course"
+        />
+      )}
+      <div className="p-6">
+        <div className="flex items-center justify-between">
+          <div className="w-full">
+            <Link
+              href={`/teacher/courses/${params.courseId}/chapters/${params.chapterId}`}
+              className="flex items-center text-sm hover:opacity-75 transition mb-6"
+            >
+              <ArrowLeft className="h-4 w-4 mr-2" />
+              Back to chapter setup
+            </Link>
+            <div className="flex items-center justify-between w-full">
+              <div className="flex flex-col gap-y-2">
+                <h1 className="text-2xl font-medium">
+                  Quiz Editing
+                </h1>
+                <span className="text-sm text-slate-700">
+                  Complete all fields {completionText}
+                </span>
+              </div>
+              {/* <QuizActions
+                disabled={!isComplete}
+                courseId={params.courseId}
+                chapterId={params.chapterId}
+                quizId={params.quizId}
+                isPublished={quiz.isPublished}
+              /> */}
+            </div>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
+          <div className="space-y-4">
+            <div>
+              <div className="flex items-center gap-x-2">
+                <IconBadge icon={Pencil} />
+                <h2 className="text-xl">
+                  Customize your quiz
+                </h2>
+              </div>
+              <QuizTitleForm
+                initialData={quiz}
+                courseId={params.courseId}
+                chapterId={params.chapterId}
+                quizId={params.quizId}
+              />
+              <QuizQuestionsForm
+                initialData={quiz.questions}
+                courseId={params.courseId}
+                chapterId={params.chapterId}
+                quizId={params.quizId}
+              />
+            </div>
+          </div>
+        </div>
+      </div >
+    </>
+  );
+}
+
+export default QuizIdPage;
