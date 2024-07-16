5a56b9eefdadde3d02f5b61db9811e60193b45b0
Gautam Sodhi <129111379+Iam-Sodhi@users.noreply.github.com>
2024-07-16 09:23:31 +0530

    #. Skeleton Added and handle no instructor message

--- app/(dashboard)/(routes)/(root)/instructors/page.tsx
+++ app/(dashboard)/(routes)/(root)/instructors/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useEffect, useState } from "react";
 import { SearchInput } from "./_components/searchInput";
 import { useDebounce } from "@/hooks/use-debounce";
@@ -6,11 +6,12 @@ import { useSearchParams } from "next/navigation";
 import axios from "axios";
 import toast from "react-hot-toast";
 import TeacherCard from "./_components/teacherCard";
+import TeacherListSkeleton from "./_components/teacherListSkeleton";
 interface Instructor {
-  id: string,
+  id: string;
   email: string;
   name: string;
-  image:string,
+  image: string;
 }
 
 interface Instructor {
@@ -20,62 +21,72 @@ interface Instructor {
 }
 
 const InstructorPage = () => {
-  const [value, setValue] = useState("")
+  const [value, setValue] = useState("");
   const debouncedValue = useDebounce(value);
   const [teachers, setTeachers] = useState<Instructor[]>([]);
-  // Use useSearchParams to get and set search parameters
+  const [loading, setLoading] = useState(true);
+
   const searchParams = useSearchParams();
   const instructorName = searchParams.get("instructor") || "";
 
-  const fetchData = async () => {
-    try {
-      const url = `/api/instructors?instructor=${instructorName}`;
-      const response = await axios.get(url);
-      setTeachers(response.data.teachers);
-      // Handle response data here
-      console.log("API Response:", response.data);
-      
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      toast.error("Error fetching Instructors");
-      // Handle errors as needed
-    }
-  };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const url = `/api/instructors?instructor=${instructorName}`;
+        const response = await axios.get(url);
+        setTeachers(response.data.teachers);
+        console.log("API Response:", response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        toast.error("Error fetching Instructors");
+        // Handle errors as needed
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [debouncedValue, instructorName]);
 
-   useEffect(()=>{
-     fetchData();
-   },[debouncedValue])
-
-   useEffect(() => {
-    console.log("Teachers:", teachers); // Log teachers whenever it changes
-  }, [teachers]);
-
-    return ( 
-      <>
-      <div >
-      <div className="w-full flex items-center justify-between px-6 bg-custom-primary h-16 sm:h-24">
-     <h1 className="text-[16px] sm:text-2xl md:text-3xl  text-white font-semibold ">Instructors</h1>
-     <SearchInput value={value} setValue={setValue} debouncedValue={debouncedValue} />
-      </div>
-      
-      <div className="p-6 space-y-4">
-
-      <div className="grid grid-cols-1 lg:grid-cols-2  xl:grid-cols-3 gap-4 ">
-          {teachers.map((teacher) => (
-            <TeacherCard
-            key={teacher.id}
-            id={teacher.id}
-            name={teacher.name}
-            email={teacher.email}
-            image={teacher.image}
+  return (
+    <>
+  
+        <div>
+          <div className="w-full flex items-center justify-between px-6 bg-custom-primary h-16 sm:h-24">
+            <h1 className="text-[16px] sm:text-2xl md:text-3xl  text-white font-semibold ">
+              Instructors
+            </h1>
+            <SearchInput
+              value={value}
+              setValue={setValue}
+              debouncedValue={debouncedValue}
             />
-          ))}
-          {/* <TeacherCard id="clxnq6x7900001whgj38s0qs2" name="John Doe" email="gautams.cs@nitj.ac.in" image="https://lh3.googleusercontent.com/a/ACg8ocJRl38nyf6EKAXOqS2pBHZvRsoFsojjOmTNc2eXpYHElQyG1ykT=s96-c" /> */}
+          </div>
+
+          <div className="p-6 space-y-4">
+          {loading ? (
+            <TeacherListSkeleton />
+          ) : teachers.length === 0 ? (
+            <div className="text-center text-sm text-muted-foreground mt-10">
+              No instructors found
+            </div>
+          ) : (
+            <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4">
+              {teachers.map((teacher) => (
+                <TeacherCard
+                  key={teacher.id}
+                  id={teacher.id}
+                  name={teacher.name}
+                  email={teacher.email}
+                  image={teacher.image}
+                />
+              ))}
+            </div>
+          )}
+          </div>
         </div>
-        </div>
-      </div>
-      </>
-     );
-  }
-   
-  export default InstructorPage;
+    
+    </>
+  );
+};
+
+export default InstructorPage;
