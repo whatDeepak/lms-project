15c5954dd7965531dbcfae3e1a17869fcb0a9e50
Deepak Kumar <139503599+whatDeepak@users.noreply.github.com>
2024-07-19 22:28:23 +0530

    Analytics-Basic-Layout

---
+++ app/(dashboard)/(routes)/teacher/analytics/courses/page.tsx
@@ -0,0 +1,13 @@
+// app/teacher/analytics/courses/page.tsx
+import React from 'react';
+
+const CoursesPage = () => {
+  return (
+    <div>
+      <h1>Courses</h1>
+      {/* Your courses content here */}
+    </div>
+  );
+};
+
+export default CoursesPage;
