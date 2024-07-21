96cc62d54bfba03a46813ebb7853dad990acdb32
Deepak Kumar <139503599+whatDeepak@users.noreply.github.com>
2024-07-21 18:51:59 +0530

    Replaced"images.domains" to "images.remotePatterns"

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: [
-          "uploadthing.com",
-          "utfs.io",
-          "lh3.googleusercontent.com",
-          "images.unsplash.com"
-        ]
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "uploadthing.com",
+        pathname: "/**"
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        pathname: "/**"
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**"
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**"
       }
+    ]
+  }
 };
 
 export default nextConfig;
